731bdedbf8d3a73beb5c04ded441737a3d48c50e
Artem Rastorguev <hizlle@mail.ru>
2020-11-28 22:36:06 +0400

    adding smart case a functional in the application

--- src/contacts/constants/StringEnum.java
+++ src/contacts/constants/StringEnum.java
@@ -1,7 +1,6 @@
 package contacts.constants;
 
 
-
 public enum StringEnum {
     /*
         Message for input
@@ -11,13 +10,25 @@ public enum StringEnum {
     ENTER_THE_NUMBER("Enter the number:"),
 
 
-    MENU("Enter action (add, remove, edit, count, list, exit):"),
+    MENU_BAR("Enter action (add, remove, edit, count, list, exit):"),
+
 
     /*
         Message for after-input
      */
     RECORD_CREATED("A record created!"),
-    PB_WITH_SINGLE_REC_CREATED("A Phone book with a %s record created!");
+    PB_WITH_SINGLE_REC_CREATED("A Phone book with a %s record created!"),
+
+    PHONE_BOOK_IS_EMPTY("The Phone Book has 0 records."),
+    NO_RECORDS_TO_EDIT("No records to edit!"),
+    NO_RECORDS_TO_REMOVE("No records to remove!"),
+    WRONG_NUMBER_FORMAT("Wrong number format!"),
+    RECORD_ADDED("The record added."),
+    SELECT_RECORD("Select a record:"),
+    UPDATE_RECORD("The record updated!"),
+    REMOVED_RECORD("The record removed!"),
+    NO_NUMBER("[no number]");
+
 
     private String stringValue;
 
