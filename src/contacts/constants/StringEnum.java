ca3231cec0013ac790232f6960d37c5b9c9f0cd8
Artem Rastorguev <hizlle@mail.ru>
2020-12-17 22:08:37 +0500

    adding case remove, using hashMap

--- src/contacts/constants/StringEnum.java
+++ src/contacts/constants/StringEnum.java
@@ -9,6 +9,8 @@ public enum StringEnum {
     ENTER_SURNAME_PERSON("Enter the surname of the person:"),
     ENTER_THE_NUMBER("Enter the number:"),
 
+    SELECT_RECORD("Select a record: "),
+    SELECT_A_FIELD("Select a field (name, surname, number):"),
 
     MENU_BAR("Enter action (add, remove, edit, count, list, exit):"),
 
@@ -17,18 +19,26 @@ public enum StringEnum {
         Message for after-input
      */
     RECORD_CREATED("A record created!"),
+
     PB_WITH_SINGLE_REC_CREATED("A Phone book with a %s record created!"),
+
     PB_WITH_OTHER_REC_CREATED("The Phone Book has %d records."),
 
 
-    PHONE_BOOK_IS_EMPTY("The Phone Book has 0 records."),
+    PHONE_BOOK_IS_CONTAINS_RECORDS("The Phone Book has %d records."),
+
     NO_RECORDS_TO_EDIT("No records to edit!"),
+
     NO_RECORDS_TO_REMOVE("No records to remove!"),
+
     WRONG_NUMBER_FORMAT("Wrong number format!"),
+
     RECORD_ADDED("The record added."),
-    SELECT_RECORD("Select a record:"),
+
     UPDATE_RECORD("The record updated!"),
+
     REMOVED_RECORD("The record removed!"),
+
     NO_NUMBER("[no number]");
 
 
